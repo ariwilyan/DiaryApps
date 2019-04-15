dfc13f1d76010ce824f57c7467c246b6f96cefc6
ariwilyan <ariwilyan@gmail.com>
2019-04-15 08:46:50 +0700

    Mengisi code pada Class ListNotesModel, ListNotesAdapter dan ListNotesActivity

--- app/src/main/java/motion/diaryapps/list_notes/ListNotesActivity.java
+++ app/src/main/java/motion/diaryapps/list_notes/ListNotesActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,8 +39,9 @@ public class ListNotesActivity extends AppCompatActivity {
         initToolbar();
 
         // TODO: 4/12/19 panggil initRecyclerView() disini
-
+        initRecyclerView();
         // TODO: 4/12/19 panggil initDummy() disini
+        initDummy();
     }
 
     /**
@@ -48,7 +50,7 @@ public class ListNotesActivity extends AppCompatActivity {
     public void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Diary Apps");
+        getSupportActionBar().setTitle("My Diarys");
     }
 
     /**
@@ -57,13 +59,13 @@ public class ListNotesActivity extends AppCompatActivity {
     public void initRecyclerView() {
         // TODO: 4/12/19 -> ganti null dengan component RecyclerView pada activity_list_notes
         // hint: gunakan findViewById(R.id.xxxxx);
-        mRecyclerView = null;
+        mRecyclerView = findViewById(R.id.rvListNotes);
 
         // TODO: 4/12/19 -> ganti null dengan objek ListNotesAdapter
-        mAdapter = null;
+        mAdapter = new ListNotesAdapter(mLists, this);
 
         // TODO: 4/12/19 -> ganti null dengan objek LinearLayoutManager
-        mLayoutManager = null;
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
 
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -77,9 +79,13 @@ public class ListNotesActivity extends AppCompatActivity {
         // TODO: 4/12/19 -> lengkapi parameter untuk ListNotesModel disini
         // hint: lakukan setelah menambahkan constructor pada ListNotesModel
         // untuk date gunakan Tools.getCurrentDateISO8601()
+        String date = Tools.getCurrentDateISO8601();
 
-        mLists.add(new ListNotesModel());
-
+        mLists.add(new ListNotesModel("1","https://t-ec.bstatic.com/images/hotel/max1024x768/136/136201154.jpg","Paradise Lost",date));
+        mLists.add(new ListNotesModel("2","https://t-ec.bstatic.com/images/hotel/max1024x768/136/136201154.jpg", "Paradise Lost",date));
+        mLists.add(new ListNotesModel("3","https://t-ec.bstatic.com/images/hotel/max1024x768/136/136201154.jpg", "Paradise Lost",date));
+        mLists.add(new ListNotesModel("4","https://t-ec.bstatic.com/images/hotel/max1024x768/136/136201154.jpg", "Paradise Lost",date));
+        mLists.add(new ListNotesModel("5","https://t-ec.bstatic.com/images/hotel/max1024x768/136/136201154.jpg", "Paradise Lost",date));
         mAdapter.notifyDataSetChanged();
     }
 
